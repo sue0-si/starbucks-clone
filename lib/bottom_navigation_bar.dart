41d18076b9f7d17b98ed23ef21a180557a1409b1
slee3593 <slee3593@gatech.edu>
2023-12-22 17:37:48 +0900

    style: nav bar 타입 & 아이콘 변경

--- lib/bottom_navigation_bar.dart
+++ lib/bottom_navigation_bar.dart
@@ -14,11 +14,11 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      type: BottomNavigationBarType.shifting,
+      type: BottomNavigationBarType.fixed,
       currentIndex: _selectedIndex,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.payment), label: 'Pay'),
+        BottomNavigationBarItem(icon: Icon(Icons.credit_card), label: 'Pay'),
         BottomNavigationBarItem(icon: Icon(Icons.local_drink), label: 'Order'),
         BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'Shop'),
         BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: 'Other'),
